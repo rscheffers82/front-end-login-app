5736e9fb7e6914bb9b847e35e053ba76d7ff8210
Roy Scheffers <r.scheffers@gmail.com>
2017-01-13 12:45:15 -0600

    build out login action generator to include redirect and authentication

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,27 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3000';
 
 export function signinUser( {email, password}) {
   return function(dispatch, state) {
     // Submit email/password to the server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-    // request is successfull...
-    // - Update state to indicate user is authenticated
-    // - Save JWT token
-    // - redirect to '/feature'
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then( response => {
+        // request is successfull...
+        // - Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
+        // - Save JWT token
+        // - redirect to '/feature'
+        browserHistory.push('/feature');
 
-    // request failed...
-    // - Show error
+      })
+      .catch( () => {
+        // request failed...
+        // - Show error
 
+      })
   }
 
 }
---
+++ src/reducers/auth_reducer.js
@@ -0,0 +1,21 @@
+import {
+  AUTH_USER,
+  UNAUTH_USER
+} from './types';
+
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case AUTH_USER:
+      return {
+        ...state,
+        authenticated: true
+      };
+    case UNAUTH_USER:
+      return {
+        ...state,
+        authenticated: false
+      };
+  }
+  return state;
+}

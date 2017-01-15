7dd10f1a8af7e0d342fee7fb0a239d3c6482927e
Roy Scheffers <r.scheffers@gmail.com>
2017-01-15 12:07:59 -0600

    Add basic landing page for default route

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk'
 
 import App from './components/app';
+import Landing from './components/landing';
 import Signup from './components/auth/signup';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
@@ -19,10 +20,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path='/' component={App}>
+        <IndexRoute component={Landing} />
         <Route path='signup' component={Signup} />
         <Route path='signin' component={Signin} />
         <Route path='signout' component={Signout} />
-        <Route path='feature' component={Feature} />
+        <Route path='feature' component={RequireAuth(Feature)} />
       </Route>
     </Router>
   </Provider>

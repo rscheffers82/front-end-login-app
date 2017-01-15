e7851f92a17756675962c233b4cb8f9d96b142b6
Roy Scheffers <r.scheffers@gmail.com>
2017-01-15 10:35:44 -0600

    Add email and additional pwd validation

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -15,15 +15,17 @@ class Signup extends Component {
         <fieldset className="form-group">
           <label>Email:</label>
           <input className="form-control" {...email}/>
+          {email.touched && email.error && <div className="error">{email.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
           <input type="password" className="form-control" {...password}/>
+          {password.touched && password.error && <div className="error">{password.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Confirm password:</label>
           <input type="password" className="form-control" {...passwordConfirm}/>
-          {password.touched && password.error && <div className="error">{password.error}</div>}
+          {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
         </fieldset>
         <button action="submit" className="btn btn-primary">Sign up</button>
       </form>
@@ -36,9 +38,17 @@ function validate(formProps) {
   // minimum requirements are that a errors object is defined and returned
   // the errors object returned, adds an error object to each form name, eg. password field error is password.error, email = email.error
   const errors = {};
+  const emailRegex = /^[A-Z0-9._%+-]+@(?:[A-Z0-9-]+\.)+[A-Z]{2,7}$/;
 
-  if (formProps.password !== formProps.passwordConfirm) {
-    errors.password = 'Ensure passwords match';
+  if (!formProps.email) errors.email = "Please provide an email";
+  if (formProps.email) {
+    if (formProps.email.toUpperCase().search(emailRegex) == -1 ) errors.email = "Please provide a valid email";
+  }
+  if (!formProps.password) errors.password = "Please provide a password";
+  if (!formProps.passwordConfirm) errors.passwordConfirm = "Please confirm your password";
+
+  if (formProps.passwordConfirm !== formProps.password) {
+    errors.passwordConfirm = 'Ensure passwords match';
   }
 
   return errors;
